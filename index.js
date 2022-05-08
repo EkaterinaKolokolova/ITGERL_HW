e4adda24b2da30d713e4ec80261622ef6a02fb83
EkaterinaKolokolova <98693231+EkaterinaKolokolova@users.noreply.github.com>
2022-05-08 20:33:49 +0300

    add all

---
+++ index.js
@@ -0,0 +1,10 @@
+function test_btn () {
+    document.location.assign ("https://www.apple.com/ru/")
+}
+
+function clearInput () {
+    const input = document.querySelector("input");
+    input.value = "";
+}
+let sayHi = () => alert ("Привет, Маша!");
+
